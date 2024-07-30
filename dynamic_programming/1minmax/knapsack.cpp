803e20b5559d2e2f6ce10775f86b164c293b081b
sourabh jadav <sourabhjadav7@gmail.com>
2024-07-30 20:21:15 +0530

    optimize space in dp [0 1 knapsack]

--- dynamic_programming/1minmax/knapsack.cpp
+++ dynamic_programming/1minmax/knapsack.cpp
@@ -59,29 +59,69 @@ int knapsackmemo(int profit[],int weight[],int **memo,int W,int n){ // [ own ]
     // }
     // std::cout<<knapsackmemo(profit,weight,memo,W,n)<<std::endl;
 
-// dynamic programming solution for the above problem
-int dpknapsack(int profit[],int weight[],int W,int n){ // [ own ]
-    int dp[n+1][W+1];
-    dp[0][0]=0;
-    memset(dp, 0, sizeof(dp)); // this is important
+// // dynamic programming solution for the above problem
+// int dpknapsack(int profit[],int weight[],int W,int n){ // [ own ]
+//     int dp[n+1][W+1];
+//     dp[0][0]=0;
+//     memset(dp, 0, sizeof(dp)); // this is important
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=W;j++){
+//             if (j<weight[i-1]) {
+//                 dp[i][j]=dp[i-1][j];
+//             }else {
+//                 dp[i][j]=max(profit[i-1]+dp[i-1][j-weight[i-1]],dp[i-1][W]);
+//             }
+//         }
+//     }
+//     return dp[n][W];
+// }
+// // since we are only considering 
+//
+// int main() {
+//     int profit[] = { 60, 100, 120 };
+//     int weight[] = { 10, 20, 30 };
+//     int W = 50;
+//     int n = sizeof(profit) / sizeof(profit[0]);
+//     std::cout<<dpknapsack(profit, weight, W, n)<<std::endl;
+//     return 0;
+// }
+
+
+
+
+// now there are three major areas we have to look at inorder to optimize the 
+// 
+// 1 optimizing space 
+// - we can maintain two arrays of size W+1 prev and current 
+// - initially prev has all the zeroes we calculate the current array 
+//   and push the value into the prev for the next index
+// 
+// - or we can use only one array initially has all 0's 
+// - and for a given i we want dp[i-1][j-weight[i-1]] // which is present on the left side
+//
+//
+
+
+// https://www.notion.so/knapsack-68bae09d9f1848ddb12bba044b3765ee?pvs=4#3d171f8079a340e49d14a2cc9d279f69
+int knapSackdpOptimized(int W, int weight[], int profit[], int n) {
+    int dp[W+1];
+    memset(dp, 0, sizeof(dp));
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=W;j++){
-            if (j<weight[i-1]) {
-                dp[i][j]=dp[i-1][j];
-            }else {
-                dp[i][j]=max(profit[i-1]+dp[i-1][j-weight[i-1]],dp[i-1][W]);
+        for(int j=W;j>0;j--){
+            if (j>=weight[i-1]) {
+                dp[j]=max(dp[j],profit[i-1]+dp[j-weight[i-1]]);
             }
         }
     }
-    return dp[n][W];
+    return dp[W];
 }
-
-
-int main() {
+// Driver code
+int main()
+{
     int profit[] = { 60, 100, 120 };
     int weight[] = { 10, 20, 30 };
     int W = 50;
     int n = sizeof(profit) / sizeof(profit[0]);
-    std::cout<<dpknapsack(profit, weight, W, n)<<std::endl;
+    cout << knapSackdpOptimized(W, weight, profit, n);
     return 0;
 }
