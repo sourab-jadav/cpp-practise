9b4a98ed4a5c1db05871afc49433ae6c354f78e0
sourabh jadav <sourabhjadav7@gmail.com>
2024-08-07 14:49:41 +0530

    lcs  [ dp ]

--- dynamic_programming/LongestIncreasingSubsequence.cpp
+++ dynamic_programming/LongestIncreasingSubsequence.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <climits>
+#include <cstring>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -15,6 +16,17 @@ using namespace std;
         }                                              \
         std::cout<<std::endl;                          \
     } while (0)
+
+#define printarray(arr, n)                             \
+        std::cout<<std::endl;                          \
+        for (int i = 0; i < n; ++i) {                  \
+            std::cout<<arr[i]<<" ";                    \
+        }                                              \
+        std::cout<<std::endl;                          \
+
+
+
+
 int solveRecursive(int arr[],int n,int i,int prev){
     if (i==n) {
         return 0;
@@ -26,12 +38,47 @@ int solveRecursive(int arr[],int n,int i,int prev){
     int exclude=solveRecursive(arr, n, i+1, prev);
     return max(include,exclude);
 }
+// tabulation solution
+// int solve_dp(int arr[],int n){
+//     int max_value=INT_MIN;
+//     for(int i=0;i<n;i++){
+//         max_value=max(max_value,arr[i]);
+//     }
+// }
+int solveDP(int arr[],int n){
+    int dp[n];
+     // the idea here to 
+     // get lis ending with the given element
+    memset(dp, 0, sizeof dp);
+    dp[0]=1;
+    // finding the element smaller than the current value on the 
+    // left side
+    int result=0;
+    for(int i=1;i<n;i++){
+        int j;
+        bool flag=false;
+        for(j=i-1;j>=0;j--){
+            if (arr[j]<=arr[i]) {
+                flag=true;
+                dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        if (!flag) {
+            dp[i]=1;
+        }
+        result=max(result,dp[i]);
+    }
+    return result;
+}
+
+
 int main() {
     // int arr[]{3,10,2,1,20};
-     int arr[]{0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
+    int arr[]{0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int result=solveRecursive(arr, n, 0, INT_MIN);
-    std::cout<<result<<std::endl;
+    // int result=solveRecursive(arr, n, 0, INT_MIN);
+    // std::cout<<result<<std::endl;
+    std::cout<<solveDP(arr, n)<<std::endl;
     return 0;
 }
 
